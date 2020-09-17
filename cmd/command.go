74cbf111a45a23b8a3ea8c64744989628c18e707
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-09-17 12:14:06 +0300

    Add cli skeleton

---
+++ cmd/command.go
@@ -0,0 +1,100 @@
+package cmd
+
+import (
+	"io"
+	"os"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/auth0ctl/cmd/create"
+	"github.com/giantswarm/auth0ctl/cmd/version"
+	"github.com/giantswarm/auth0ctl/pkg/project"
+)
+
+type Config struct {
+	Logger micrologger.Logger
+	Stderr io.Writer
+	Stdout io.Writer
+
+	BinaryName string
+	GitCommit  string
+	Source     string
+}
+
+func New(config Config) (*cobra.Command, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+	if config.Stderr == nil {
+		config.Stderr = os.Stderr
+	}
+	if config.Stdout == nil {
+		config.Stdout = os.Stdout
+	}
+
+	if config.GitCommit == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.GitCommit must not be empty", config)
+	}
+	if config.Source == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Source must not be empty", config)
+	}
+
+	var err error
+
+	var createCmd *cobra.Command
+	{
+		c := create.Config{
+			Logger: config.Logger,
+			Stderr: config.Stderr,
+			Stdout: config.Stdout,
+		}
+
+		createCmd, err = create.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var versionCmd *cobra.Command
+	{
+		c := version.Config{
+			Logger: config.Logger,
+			Stderr: config.Stderr,
+			Stdout: config.Stdout,
+
+			GitCommit: config.GitCommit,
+			Source:    config.Source,
+		}
+
+		versionCmd, err = version.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	f := &flag{}
+
+	r := &runner{
+		flag:   f,
+		logger: config.Logger,
+		stderr: config.Stderr,
+		stdout: config.Stdout,
+	}
+
+	c := &cobra.Command{
+		Use:          project.Name(),
+		Short:        project.Description(),
+		Long:         project.Description(),
+		RunE:         r.Run,
+		SilenceUsage: true,
+	}
+
+	f.Init(c)
+
+	c.AddCommand(createCmd)
+	c.AddCommand(versionCmd)
+
+	return c, nil
+}
---
+++ internal/env/env.go
@@ -0,0 +1,6 @@
+package env
+
+const (
+	Auth0ClientID     = "AUTH0_CCLIENT_ID"     // nolint:gosec
+	Auth0ClientSecret = "AUTH0_CCLIENT_SECRET" // nolint:gosec
+)
