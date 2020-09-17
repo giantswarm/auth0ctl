ac0e7d2053a95ab69d2b4406e6f5733371eda487
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-09-17 15:48:47 +0300

    Add auth0 authorization

--- pkg/auth0/auth0.go
+++ pkg/auth0/auth0.go
@@ -1,1 +1,103 @@
 package auth0
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/giantswarm/microerror"
+)
+
+var (
+	managementAudience = "https://%s.eu.auth0.com/api/v2/"
+)
+
+type Config struct {
+	// credentials
+	ClientID     string
+	ClientSecret string
+
+	Tenant string
+}
+
+type Auth0 struct {
+	AccessToken string
+	Audience    string
+
+	httpClient *http.Client
+}
+
+func New(config Config) (*Auth0, error) {
+
+	if config.ClientID == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ClientID must not be empty", config)
+	}
+	if config.ClientSecret == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ClientSecret must not be empty", config)
+	}
+	if config.Tenant == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Tenant must not be empty", config)
+	}
+
+	accessToken, err := getAccessToken(config.ClientID, config.ClientSecret, config.Tenant)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	httpClient := &http.Client{}
+
+	return &Auth0{
+		AccessToken: accessToken,
+		Audience:    fmt.Sprintf(managementAudience, config.Tenant),
+
+		httpClient: httpClient,
+	}, nil
+}
+
+func getAccessToken(appID, appSecret, tenant string) (string, error) {
+	httpClient := &http.Client{}
+
+	authEndpoint := fmt.Sprintf("https://%s.eu.auth0.com/oauth/token", tenant)
+
+	data := url.Values{}
+	data.Set("grant_type", "client_credentials")
+	data.Set("audience", fmt.Sprintf(managementAudience, tenant))
+	data.Set("client_id", appID)
+	data.Set("client_secret", appSecret)
+
+	req, err := http.NewRequest("POST", authEndpoint, strings.NewReader(data.Encode()))
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	type auth0LoginResponse struct {
+		AccessToken string `json:"access_token"`
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	var auth0LoginData auth0LoginResponse
+
+	err = json.Unmarshal(body, &auth0LoginData)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	return auth0LoginData.AccessToken, nil
+}
--- pkg/auth0/error.go
+++ pkg/auth0/error.go
@@ -2,22 +2,11 @@ package auth0
 
 import "github.com/giantswarm/microerror"
 
-// Indicates that the release was not found for the given provider and version.
-var releaseNotFoundError = &microerror.Error{
-	Kind: "releaseNotFoundError",
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
 }
 
-// IsInvalidConfig asserts releaseNotFoundError.
-func IsReleaseNotFound(err error) bool {
-	return microerror.Cause(err) == releaseNotFoundError
-}
-
-// Indicates that the component or app is incorrectly formatted.
-var badFormatError = &microerror.Error{
-	Kind: "badFormatError",
-}
-
-// IsBadFormat asserts badFormatError.
-func IsBadFormat(err error) bool {
-	return microerror.Cause(err) == badFormatError
+// IsInvalidConfig asserts invalidConfigError.
+func IsInvalidConfig(err error) bool {
+	return microerror.Cause(err) == invalidConfigError
 }
