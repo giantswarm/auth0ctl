eb6325e729959b23d244a250e6e4cda7719d3dfb
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-09-28 01:26:19 +0300

    Update

--- cmd/update/client/flag.go
+++ cmd/update/client/flag.go
@@ -7,18 +7,36 @@ import (
 )
 
 const (
-	flagID     = "id"
-	flagTenant = "tenant"
+	flagID                     = "id"
+	flagTenant                 = "tenant"
+	flagAddCallback            = "add-callback"
+	flagAddAllowedLogoutURL    = "add-allowed-logout-url"
+	flagAddWebOrigin           = "add-web-origin"
+	flagRemoveAllowedLogoutURL = "remove-allowed-logout-url"
+	flagRemoveCallback         = "remove-callback"
+	flagRemoveWebOrigin        = "remove-web-origin"
 )
 
 type flag struct {
-	ID     string
-	Tenant string
+	ID                     string
+	Tenant                 string
+	AddCallback            []string
+	AddAllowedLogoutURL    []string
+	AddWebOrigin           []string
+	RemoveCallback         []string
+	RemoveAllowedLogoutURL []string
+	RemoveWebOrigin        []string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&f.ID, flagID, "", "ID of the client to update.")
 	cmd.Flags().StringVar(&f.Tenant, flagTenant, "giantswarm", `Auth0 tenant.`)
+	cmd.Flags().StringSliceVar(&f.AddCallback, flagAddCallback, []string{}, "Callback to be added to the client.")
+	cmd.Flags().StringSliceVar(&f.AddAllowedLogoutURL, flagAddAllowedLogoutURL, []string{}, "Allowed logout url to be added to the client.")
+	cmd.Flags().StringSliceVar(&f.AddWebOrigin, flagAddWebOrigin, []string{}, "Web origin to be added to the client.")
+	cmd.Flags().StringSliceVar(&f.RemoveCallback, flagRemoveCallback, []string{}, "Callback to be removed to the client.")
+	cmd.Flags().StringSliceVar(&f.RemoveAllowedLogoutURL, flagRemoveAllowedLogoutURL, []string{}, "Allowed logout url to be removed to the client.")
+	cmd.Flags().StringSliceVar(&f.RemoveWebOrigin, flagRemoveWebOrigin, []string{}, "Web origin to be removed to the client.")
 }
 
 func (f *flag) Validate() error {
--- cmd/update/client/runner.go
+++ cmd/update/client/runner.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"io"
 
 	"github.com/giantswarm/microerror"
@@ -54,5 +55,51 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 		return microerror.Mask(err)
 	}
 
+	newCallbacks := r.flag.AddCallback
+	{
+		for _, callback := range client.Callbacks {
+			if !stringInSlice(callback, r.flag.RemoveCallback) {
+				newCallbacks = append(newCallbacks, callback)
+			}
+		}
+	}
+	client.Callbacks = newCallbacks
+
+	newAllowedLogoutURLs := r.flag.AddAllowedLogoutURL
+	{
+		for _, allowedLogoutURL := range client.AllowedLogoutURLs {
+			if !stringInSlice(allowedLogoutURL, r.flag.RemoveAllowedLogoutURL) {
+				newAllowedLogoutURLs = append(newAllowedLogoutURLs, allowedLogoutURL)
+			}
+		}
+	}
+	client.AllowedLogoutURLs = newAllowedLogoutURLs
+
+	newWebOrigins := r.flag.AddWebOrigin
+	{
+		for _, webOrigin := range client.WebOrigins {
+			if !stringInSlice(webOrigin, r.flag.RemoveWebOrigin) {
+				newWebOrigins = append(newWebOrigins, webOrigin)
+			}
+		}
+	}
+	client.WebOrigins = newWebOrigins
+
+	err = a0.UpdateClient(r.flag.ID, client)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	fmt.Printf("Auth0 client with ID %#q in tenant %#q  was successfully updated.\n", r.flag.ID, r.flag.Tenant)
+
 	return nil
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
